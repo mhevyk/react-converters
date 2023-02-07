65a581368dd4cfcb6d65c644c295a5489bb80cc9
mhevyk <maxxevyk7@gmail.com>
2023-02-07 11:34:03 +0200

    Added possibility to close sidebar when link is clicked

--- src/components/ConverterTypesNavbar/index.tsx
+++ src/components/ConverterTypesNavbar/index.tsx
@@ -1,13 +1,22 @@
 import { Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
-function ConverterTypesNav() {
+interface ConverterTypesNavProps {
+  onLinkClick: () => void;
+}
+
+function ConverterTypesNav({ onLinkClick }: ConverterTypesNavProps) {
   const converterTypes: string[] = ['area', 'length', 'temperature'];
 
   return (
     <Nav className="justify-content-end flex-grow-1 pe-3">
       {converterTypes.map((type) => (
-        <NavLink key={type} to={`/${type}`} className="nav__link">
+        <NavLink
+          key={type}
+          to={`/${type}`}
+          className="nav__link"
+          onClick={onLinkClick}
+        >
           {type}
         </NavLink>
       ))}
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,35 +2,50 @@ import './Menu.css';
 import { Navbar, Container, Offcanvas } from 'react-bootstrap';
 import ConverterTypesNavbar from '../ConverterTypesNavbar';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Menu() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+
   const expand: string = 'md';
   const offCanvasNavbarClassName: string = `offcanvasNavbar-expand-${expand}`;
   const offCanvasNavbarLabelClassName: string = `offcanvasNavbarLabel-expand-${expand}`;
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const hideSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
     <Navbar
       bg="light"
+      expanded={isSidebarOpen}
       expand={expand}
       className="mb-3 border-bottom border-secondary"
     >
       <Container fluid>
         <Navbar.Brand as={Link} to="/">
-          Types
+          <img src="assets/logo.png" alt="logo" className="logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls={offCanvasNavbarClassName} />
+        <Navbar.Toggle
+          aria-controls={offCanvasNavbarClassName}
+          onClick={toggleSidebar}
+        />
         <Navbar.Offcanvas
           id={offCanvasNavbarClassName}
           aria-labelledby={offCanvasNavbarLabelClassName}
           placement="end"
         >
-          <Offcanvas.Header closeButton>
+          <Offcanvas.Header closeButton onHide={hideSidebar}>
             <Offcanvas.Title id={offCanvasNavbarLabelClassName}>
               Converters
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <ConverterTypesNavbar />
+            <ConverterTypesNavbar onLinkClick={hideSidebar} />
           </Offcanvas.Body>
         </Navbar.Offcanvas>
       </Container>
