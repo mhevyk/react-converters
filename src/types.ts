6ac22b689b4f5b1110acd9963fe0c1c33a655ffe
mhevyk <maxxevyk7@gmail.com>
2023-02-10 14:15:21 +0200

    Changed api structure

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,19 @@
+export enum ConverterTypes {
+  AREA = "area",
+  LENGTH = "length",
+  TEMPERATURE = "temperature"
+}
+
 export interface IMeasurementUnit {
-    id: string;
-    value: number;
-    names: {
-      short?: string;
-      full: string;
-    };
-    group?: string;
-  }
+  id: string;
+  value: number;
+  names: {
+    short?: string;
+    full: string;
+  };
+}
+
+export interface IMeasurementUnitGroup {
+  name: string,
+  content: IMeasurementUnit[]
+}
