769f27c0c41bd99a7d5750803382ca3b6a829deb
mhevyk <maxxevyk7@gmail.com>
2023-03-13 20:34:06 +0200

    Edited home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,31 @@
-import { Navigate } from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import sitemap from '../routes/sitemap';
 
 function HomePage() {
   return (
-    <Navigate to='/area' />
-  )
+    <Container fluid="md" className="px-3">
+      <h2>Welcome to Converters!</h2>
+      <p>Select one of converters to start your work:</p>
+      <ul className="list-group list-group-flush">
+        {sitemap.map(
+          (route) =>
+            route.name && (
+              <li key={route.path} className="list-group-item bg-transparent">
+                {route?.icon && (
+                  <span className="me-4">
+                    <route.icon size={25} />
+                  </span>
+                )}
+                <Link to={route.path} className="text-primary">
+                  {route.name}
+                </Link>
+              </li>
+            )
+        )}
+      </ul>
+    </Container>
+  );
 }
 
 export default HomePage;
