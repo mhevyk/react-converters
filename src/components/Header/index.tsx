9e592cfa8eba9e34a8676444191039d053270255
mhevyk <maxxevyk7@gmail.com>
2023-03-13 20:32:09 +0200

    Improved centering of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,14 @@ import './Header.css';
 function Header() {
   return (
     <header className="App__header">
-      <h1 className="title text-primary text-center text-uppercase">
-        Converters
-      </h1>
-      <h3 className="subtitle text-white text-center text-uppercase">
-        Handy enough to use it!
-      </h3>
+      <div className="header__main">
+        <h1 className="title text-primary text-center text-uppercase">
+          Converters
+        </h1>
+        <h3 className="subtitle text-white text-center text-uppercase">
+          Handy enough to use it!
+        </h3>
+      </div>
     </header>
   );
 }
